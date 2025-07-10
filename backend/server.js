bd434badba0b75171f3265ba433adc59b349828b
immza <moinuddinzubair26@gmail.com>
2025-07-10 12:41:42 +0600

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //const express = require('express');
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 //import mongoose from 'mongoose';
 import { connectDB } from "./config/db.js";
 //import Product from './models/product.model.js';
@@ -13,9 +14,19 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //allows us to use JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
